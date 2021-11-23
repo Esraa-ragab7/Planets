a5fd3ba691f8ed7f4e3d11060ed12255202e8ca7
Esraa Azab <sara2_fun2010@yahoo.com>
2021-11-23 20:24:07 +0200

    Add alert method

--- Planets/Extensions/UIViewController+Extensions/UIViewController+Alert.swift
+++ Planets/Extensions/UIViewController+Extensions/UIViewController+Alert.swift
@@ -5,4 +5,16 @@
 //  Created by Esraa Mohamed Ragab on 23/11/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func presentAlert(withTitle title:String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
