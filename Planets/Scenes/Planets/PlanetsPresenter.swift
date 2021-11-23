49b7a3bac94cef68c932abe0d1b6c32f25bb06b0
Esraa Azab <sara2_fun2010@yahoo.com>
2021-11-23 21:31:01 +0200

    Add Planets Presenter and add view controller datasource delegate.

--- Planets/Scenes/Planets/PlanetsPresenter.swift
+++ Planets/Scenes/Planets/PlanetsPresenter.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+protocol PlanetsView: AnyObject {
+    func refreshPlanetsView()
+    func displayPlanetsRetrievalError(title: String, message: String)
+}
+
 protocol PlanetCellView {
     func display(title: String)
+}
+
+protocol PlanetsPresenter {
+    var numberOfPlanets: Int { get }
+    func viewDidLoad()
+    func configure(cell: PlanetCellView, forRow row: Int)
+}
+
+class PlanetsPresenterImplementation: PlanetsPresenter {
+    var numberOfPlanets: Int = 0
+    
+    func viewDidLoad() {
+        
+    }
+    
+    func configure(cell: PlanetCellView, forRow row: Int) {
+        
+    }
 }
