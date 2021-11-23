83519b56e1ed310f87840b975ebd503757baa948
Esraa Azab <sara2_fun2010@yahoo.com>
2021-11-23 19:47:41 +0200

    Add scenes group, EntityGateway and Core Groups

---
+++ Planets/Scenes/Planets/PlanetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlanetTableViewCell.swift
+//  Planets
+//
+//  Created by Esraa Mohamed Ragab on 23/11/2021.
+//
+
+import UIKit
+
+class PlanetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
