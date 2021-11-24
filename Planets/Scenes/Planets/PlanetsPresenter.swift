0507949bf843c947957c316edfd3c65e8951c24e
Esraa Azab <sara2_fun2010@yahoo.com>
2021-11-24 02:24:09 +0200

    Complete the presenter

--- Planets/Scenes/Planets/PlanetsPresenter.swift
+++ Planets/Scenes/Planets/PlanetsPresenter.swift
@@ -23,13 +23,49 @@ protocol PlanetsPresenter {
 }
 
 class PlanetsPresenterImplementation: PlanetsPresenter {
-    var numberOfPlanets: Int = 0
+    fileprivate weak var view: PlanetsView?
+    fileprivate let displayPlanetsUseCase: DisplayPlanetsUseCase
+    
+    // we keep this internal so we can inject values for testing purposes
+    var planets = [Planet]()
+    
+    var numberOfPlanets: Int {
+        return planets.count
+    }
+    
+    init(view: PlanetsView,
+         displayPlanetsUseCase: DisplayPlanetsUseCase) {
+        self.view = view
+        self.displayPlanetsUseCase = displayPlanetsUseCase
+    }
     
     func viewDidLoad() {
-        
+        self.displayPlanetsUseCase.displayPlanets { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case let .success(planets):
+                self.handlePlanetsReceived(planets)
+            case let .failure(error):
+                self.handlePlanetsError(error)
+            }
+        }
     }
     
     func configure(cell: PlanetCellView, forRow row: Int) {
+        let planet = planets[row]
         
+        cell.display(title: planet.name)
+    }
+    
+    // MARK: - Private Functions
+    
+    fileprivate func handlePlanetsReceived(_ planets: [Planet]) {
+        self.planets = planets
+        view?.refreshPlanetsView()
+    }
+    
+    fileprivate func handlePlanetsError(_ error: Error) {
+        // Here we could check the error code and display a localized error message
+        view?.displayPlanetsRetrievalError(title: "Error", message: error.localizedDescription)
     }
 }
